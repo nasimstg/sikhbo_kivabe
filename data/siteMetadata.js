abd94ccd4e524ebcf6b7bb6baafe13aec19bdfcf
Mohammed Nasim <75156273+nasimstg@users.noreply.github.com>
2023-01-24 19:22:50 +0600

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -16,15 +16,15 @@ const siteMetadata = {
   facebook: 'https://facebook.com/mohammed.nasim.on3',
   youtube: 'https://youtube.com/@wooenestudio',
   linkedin: 'https://www.linkedin.com/in/nasimStg',
-  locale: 'en-US',
+  locale: ['bn-BD', 'en-US'],
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    plausibleDataDomain: 'sikhbokivabe.vercel.app', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'G-582J42CPJS', // e.g. UA-000000-2 or G-XXXXXXX
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
   },
   newsletter: {
